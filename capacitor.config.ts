b8af21f503f206f7597b5ebb4fd6c6e4abf88ac1
MiraDimitrijevic <marinadimitrijevic03@gmail.com>
2024-02-22 13:36:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'beauty-salon-mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
